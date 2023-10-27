47f49d55e0033f9ed5088c2add47e925a376f630
Sandalots <sandalotsq@gmail.com>
2023-10-27 15:53:48 +0100

    Init js boilerplate

--- PoC/js/script.js
+++ PoC/js/script.js
@@ -1,9 +1,15 @@
+$(document).ready(function () {
 // JavaScript source file for Team of Interest Proof-of-Concept
 
 // Section hiding method
 
 // Restart button method
 
+$(".restartBtn").click(function () {
+    location.reload();
+});
+
 // Get user input method
 
 // Test methods
+});
