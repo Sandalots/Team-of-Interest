2f2b143a516c4e73f63f2e3750391fba3ea68955
Sandy MacDonald <59518103+Sandalots@users.noreply.github.com>
2023-10-28 19:50:32 +0100

    Test navigation

---
+++ PoC/js/output.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+
+    // if restartBtn is clicked redirect to index.html page
+    $('#retryBtn').click(function() {
+        // redirect to index.html
+        window.location.href = "index.html";
+    });
+
+    // if exitBtn is clicked destroy the window
+    $('#exitBtn').click(function() {
+        // destroy window
+        window.close();
+    });
+
+});
--- PoC/js/script.js
+++ PoC/js/script.js
@@ -1,4 +1,51 @@
-// jquery onload    
-$(function() {
-    console.log("jquery onload");
+// jquery
+$(document).ready(function() {
+    // hide personalSection class
+    $('.personalSection').hide();
+
+    // hide financialSection
+    $('.financialSection').hide();
+
+    // hide financialAssetsSection
+    $('.financialAssetsSection').hide();
+
+    // hide allDoneSection
+    $('.allDoneSection').hide(); 
+
+    // if nextBtn id is clicked
+    $('#welcomeBtn').click(function() {
+        // show financialSection
+        $('.personalSection ').show();
+    });
+    
+    // if personalBtn id is clicked
+    $('#personalBtn').click(function() {
+        // show financialSection
+        $('.financialSection').show();
+    });
+
+    // if financialInformationBtn id is clicked
+    $('#financialInformationBtn').click(function() {
+        // show financialAssetsSection
+        $('.financialAssetsSection').show();
+    });
+
+    // if financialAssetsBtn id is clicked
+    $('#financialAssetsBtn').click(function() {
+        // show allDoneSection
+        $('.allDoneSection').show();
+    });
+
+    // if restartBtn id is clicked
+    $('#restartBtn').click(function() {
+        // reload page
+        location.reload();
+    });
+
+    // if yesBtn is clicked redirect to output.html
+    $('#yesBtn').click(function() {
+        // redirect to output.html
+        window.location.href = "output.html";
+    });
+
 });
