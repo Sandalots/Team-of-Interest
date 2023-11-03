2e0133db5351cc2ae6b171a6c812705fe05cf016
Almerin0 <feher.almos18@gmail.com>
2023-11-03 15:10:38 +0000

    Try 2

--- PoC/js/script.js
+++ PoC/js/script.js
@@ -1,5 +1,25 @@
 // jquery script for the PoC, currently justs initially hides the forms until each previous form next button is clicked then shows the desired section.
 $(document).ready(function() {
+    var totalSections = 4; // Total number of sections in your process
+
+    // Update progress bar based on completed sections
+    function updateProgressBar(completedSections) {
+        var progress = (completedSections / totalSections) * 100;
+        $('.progress').css('width', progress + '%');
+    }
+
+    // Initial progress bar setup (no sections completed)
+    updateProgressBar(0);
+
+    // Function to show a specific section by its class name
+    function show(sectionClass) {
+        $(sectionClass).show();
+        updateProgressBar($(sectionClass).length); // Update progress bar after showing the section
+    }
+
+    // show welcomeSection
+    $('.welcomeSection ').show();
+
     // hide personalSection class
     $('.personalSection').hide();
 
@@ -14,26 +34,62 @@ $(document).ready(function() {
 
     // if nextBtn id is clicked
     $('#welcomeBtn').click(function() {
+        // hide welcomeSection
+        $('.welcomeSection ').hide();
         // show financialSection
         $('.personalSection ').show();
+        // hide financialSection
+        $('.financialSection').hide();
+        // hide financialAssetsSection
+        $('.financialAssetsSection').hide();
+        // hide allDoneSection
+        $('.allDoneSection').hide();
+        updateProgressBar(1);
     });
     
     // if personalBtn id is clicked
     $('#personalBtn').click(function() {
+        // hide welcomeSection
+        $('.welcomeSection ').hide();
+        // hide financialSection
+        $('.personalSection ').hide();
         // show financialSection
         $('.financialSection').show();
+        // hide financialAssetsSection
+        $('.financialAssetsSection').hide();
+        // hide allDoneSection
+        $('.allDoneSection').hide();
+        updateProgressBar(2);
     });
 
     // if financialInformationBtn id is clicked
     $('#financialInformationBtn').click(function() {
+        // hide welcomeSection
+        $('.welcomeSection ').hide();
+        // hide financialSection
+        $('.personalSection ').hide();
+        // hide financialSection
+        $('.financialSection').hide();
         // show financialAssetsSection
         $('.financialAssetsSection').show();
+        // hide allDoneSection
+        $('.allDoneSection').hide();
+        updateProgressBar(3);
     });
 
     // if financialAssetsBtn id is clicked
     $('#financialAssetsBtn').click(function() {
+        // hide welcomeSection
+        $('.welcomeSection ').hide();
+        // hide financialSection
+        $('.personalSection ').hide();
+        // hide financialSection
+        $('.financialSection').hide();
+        // hide financialAssetsSection
+        $('.financialAssetsSection').hide();
         // show allDoneSection
         $('.allDoneSection').show();
+        updateProgressBar(4);
     });
 
     // if restartBtn id is clicked
